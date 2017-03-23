f065c75836f34023ad7b18ceffd1676fda4b4a5f
wutingkang <wutingkang1@gmail.com>
2017-03-23 22:59:54 +0800

    bunengtijaio?

--- app/src/main/java/com/doit/wutingkang/learningassistant/MainActivity.java
+++ app/src/main/java/com/doit/wutingkang/learningassistant/MainActivity.java
@@ -1,32 +1,31 @@
 package com.doit.wutingkang.learningassistant;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.wifi.WifiManager;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by wutingkang on 2017/3/20.
  */
 
 public class MainActivity extends AppCompatActivity {
-    public static int LOGIN_SUCCESS = 11;
-    public static int USERNAME_ERROR = 22;
-    public static int PASSWORD_ERROR = 33;
-    public static int UNSURE = 44;
-
     private EditText editName;
     private EditText editPassWord;
-    private static Button btnLogin;
-    SharedPreferences spsLogin;
-    SharedPreferences.Editor spsEditorLogin;
+    private static Button btnLogin;//还承担着提示wifi网络状态的功能
+    private SharedPreferences.Editor spsEditorLogin;
+    private SharedPreferences spsLogin;
 
+    //传递给wifi处理广播，用于改变按钮文字
+    public static Button getLoginButton(){
+        return btnLogin;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,39 +44,34 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //判断wifi的状态，实现一键连接
-                WifiManager manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                if (! manager.isWifiEnabled()) {
-                    manager.setWifiEnabled(true);//打开wifi再连接
-                }
-
+                //保存当前数据
                 spsEditorLogin.putString("userName", editName.getText().toString());
                 spsEditorLogin.putString("passWord", editPassWord.getText().toString());
                 spsEditorLogin.apply();
-                btnLogin.setText("正在登录...");
-                new Thread(WifiInfoReceiver.initRunnable(handlerMain, MainActivity.this,
-                        editName.getText().toString(), editPassWord.getText().toString())).start();
+
+
+                WifiManager manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+                if (btnLogin.getText().equals("登录成功,再次点击退出")){
+                    if (manager.isWifiEnabled())//必须确定是已登陆时才能退出，不能仅仅是wifi连接
+
+
+                        finish();
+                    else
+                        Toast.makeText(MainActivity.this, "登录已断开，正在重新登录", Toast.LENGTH_SHORT).show();
+                }else {//需放在else里， 否则finish（）之后还会执，不知道为什么
+                    btnLogin.setText("登录中...");
+
+                    //判断wifi的状态，实现一键连接并登录，如果需要的话
+                    if (! manager.isWifiEnabled()) {
+                        manager.setWifiEnabled(true);//打开wifi,剩下的工作交给WifiInfoReceiver去做
+                    }else{
+                        //如果已经打开Wifi,发送自定义广播给WifiInfoReceiver
+                        Intent broadcastIntent = new Intent("com.doit.wutingkang.learningassistant.LOGIN_BROADCAST");
+                        sendBroadcast(broadcastIntent);
+                    }
+                }
             }
         });
-
     }
-
-    public Handler handlerMain = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-
-            if (msg.what == LOGIN_SUCCESS) {
-                 btnLogin.setText("登录成功,请关闭此页面");
-            } else if (msg.what == USERNAME_ERROR) {
-                 btnLogin.setText("用户名错误");
-                editName.setError("用户名错误");
-            } else if (msg.what == PASSWORD_ERROR) {
-                 btnLogin.setText("登录失败,密码错误");
-                editPassWord.setError("密码错误");
-            }else if (msg.what == UNSURE){
-                 btnLogin.setText("不知道连接是否成功，，，");
-            }
-        }
-    };
 }
--- app/src/main/java/com/doit/wutingkang/learningassistant/WifiInfoReceiver.java
+++ app/src/main/java/com/doit/wutingkang/learningassistant/WifiInfoReceiver.java
@@ -1,5 +1,6 @@
 package com.doit.wutingkang.learningassistant;
 
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.annotation.TargetApi;
 import android.app.Notification;
@@ -8,13 +9,15 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
-import android.os.Parcelable;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -27,6 +30,8 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 
+import static android.media.AudioManager.RINGER_MODE_VIBRATE;
+
 /**
  * Created by wutingkang on 2017/3/20.
  */
@@ -36,6 +41,11 @@ public class WifiInfoReceiver extends BroadcastReceiver {
     private NotificationManager notifyManager;
     private static final int NOTIFICATION_ID = 1;
 
+    public static int LOGIN_SUCCESS = 11;
+    public static int USERNAME_ERROR = 22;
+    public static int PASSWORD_ERROR = 33;
+    public static int UNSURE = 44;
+
     private static final String TAG = "wifiReceiver";
 
 
@@ -43,35 +53,76 @@ public class WifiInfoReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         this.context = context;
-        //没有登录则才login
 
-
-        login(context, intent);
+        //需要登录的wifi才login
+        if (needLogin(context, intent)) {
+            login(context);
+        }
     }
 
-    public void login(final Context context, Intent intent) {
+    //必须是wifi *连接上* 了某个 *需要登陆* 且 *还没有登录* 的网络时才发送登录请求
+    public static boolean needLogin(final Context context, Intent intent){
+        //wifi连接上与否
+        if(intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)){
+
+            NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+
+            if(info.getState().equals(NetworkInfo.State.CONNECTED)){  //*连接上*
+                WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+                //因为登录的用户名密码只对校园网有效，就直接根据wifi名称判断是否需要连接。(可以增加需登录的wifi名单增删改功能)
+
+                System.out.println("连接到网络: " + wifiInfo.getSSID());
+
+                if (wifiInfo.getSSID().equals("\"405\"") || //注意getSSID()返回值带有 ”“！！！， 而且直接用 == 比较不行
+                    wifiInfo.getSSID().equals("\"BUPT-portal\"")){
+
+                    System.out.println("inin");
+
+                    Button btnLogin = MainActivity.getLoginButton();
+                    if (btnLogin.getText().equals("登录") ||              //*还没有登录*
+                        btnLogin.getText().equals("登录中...")) {
+
+                        System.out.println("true");
+                        return true;
+                    }
+                }
+            }
+        }else if(intent.getAction().equals(WifiManager.WIFI_STATE_CHANGED_ACTION)){//wifi打开与否
+            int wifistate = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_DISABLED);
+
+            if(wifistate == WifiManager.WIFI_STATE_DISABLED){
+                //关闭了wifi开关，更改按钮文字
+                Button btnLogin = MainActivity.getLoginButton();
+                btnLogin.setText("登录");
+
+                System.out.println("系统关闭wifi");
+            }
+            else if(wifistate == WifiManager.WIFI_STATE_ENABLED){//打开wifi开关不用做什么事
+                System.out.println("系统开启wifi");
+            }
+        }else if (intent.getAction().equals("com.doit.wutingkang.learningassistant.LOGIN_BROADCAST")){ //自定义广播，模拟wifi状态发生改变
+            //很遗憾，在Wifi已经打开的情况下，发送这条广播无法获得Wifi的信息（因为intent是自定义发送的）进行 *需登录* 判断，只好默认是true
+
+
+            //或许可以通过http返回码来确定是否需要登录
+            return true;
+        }
+
+        return  false;
+    }
+
+
+
+    public void login(final Context context) {
         SharedPreferences spsLogin;
         String userName, passWord;
 
-        if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent.getAction())) {
-            final Parcelable parcelableExtra = intent
-                    .getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-            if (null != parcelableExtra) {
-                NetworkInfo networkInfo = (NetworkInfo) parcelableExtra;
-                NetworkInfo.State state = networkInfo.getState();
-                boolean isConnected = state == NetworkInfo.State.CONNECTED;
-                if (isConnected) {
-                    Log.i(TAG, "onReceive: 连接上可用的WIFi"); // 开始登录
-
-                    spsLogin = context.getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
-                    userName = spsLogin.getString("userName", "");
-                    passWord = spsLogin.getString("passWord", "");
-                    new Thread(initRunnable(handler, context, userName, passWord)).start();
-                } else {
-                    Log.i(TAG, "onReceive: 未连接可用wifi");
-                }
-            }
-        }
+        spsLogin = context.getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
+        userName = spsLogin.getString("userName", "");
+        passWord = spsLogin.getString("passWord", "");
+        new Thread(initRunnable(handlerWifi, context, userName, passWord)).start();
     }
 
     public static Runnable initRunnable(final Handler handler, final Context context, final String userName, final String passWord) {
@@ -83,8 +134,10 @@ public class WifiInfoReceiver extends BroadcastReceiver {
 
                 //根据登陆网页的http报文提交的参数确定
                 String parameter = "DDDDD=" + userName +
-                                    "&upass=" + passWord +
-                                    "&0MKKey=" + "";
+                                   "&upass=" + passWord +
+                                   "&0MKKey=" + "";
+
+                System.out.println("in run");
 
                 HttpURLConnection connection;
                 URL url;
@@ -114,31 +167,47 @@ public class WifiInfoReceiver extends BroadcastReceiver {
 
                     Log.i(TAG, "connection 应该发送了");
 
+
                     int responseCode = connection.getResponseCode();
                     if (responseCode == HttpURLConnection.HTTP_OK) {
-                        String s = getHtml(connection.getInputStream(), "GBK");
-                        String errorMsg = new String(s.getBytes("GBK"), "UTF-8");
+                        String strHTMLgbk = getHtml(connection.getInputStream(), "GBK");
+                        String strHTMLutf8 = new String(strHTMLgbk.getBytes("GBK"), "UTF-8");//按照utf-8编码转为java字符串
 
-                        System.out.println("Html" + s);
+                        System.out.println("Html" + strHTMLgbk);
 
                         //不同学校网页返回的内容可能不一样
-                        if (s.contains("您已经成功登录")) {//登录成功后返回的网页中的文字
-                            handler.sendEmptyMessage(MainActivity.LOGIN_SUCCESS);
-                        } else if (errorMsg.contains("ldap auth error")) {//密码错误
-                            handler.sendEmptyMessage(MainActivity.PASSWORD_ERROR);
-                        } else if (errorMsg.contains("账号或密码不对")) {//账号错误
-                            handler.sendEmptyMessage(MainActivity.USERNAME_ERROR);
+                        if (strHTMLgbk.contains("您已经成功登录")) {//登录成功后返回的网页中的文字
+                            //连wifi时一般是在教室，顺便把手机调成静音，尊重他人，避免尴尬
+                            AudioManager audioManager =  (AudioManager)context.getSystemService(Service.AUDIO_SERVICE);
+                            audioManager.setRingerMode(RINGER_MODE_VIBRATE);//铃音为静音，启动震动
+
+                            //本来想在这里改变登录按钮的文字，但发现会抛出异常，不能在其他线程改变UI线程的界面,(不过在广播里可以)
+
+                            handler.sendEmptyMessage(LOGIN_SUCCESS);
+                        } else if (strHTMLutf8.contains("ldap auth error")) {//密码错误
+                            handler.sendEmptyMessage(PASSWORD_ERROR);
+                        } else if (strHTMLutf8.contains("账号或密码不对")) {//账号错误
+                            handler.sendEmptyMessage(USERNAME_ERROR);
                         }else{
                             Log.i(TAG, "connection 其他UNSURE");
 
-                            handler.sendEmptyMessage(MainActivity.UNSURE);
+                            handler.sendEmptyMessage(UNSURE);
                         }
 
                         Log.i(TAG, "过了handler");
 
                         connection.disconnect();
+                    }else{
+                        System.out.println("should be toast");
+
+                        Toast.makeText(context, "连接到服务器出错了！", Toast.LENGTH_SHORT).show();
                     }
+
+                    System.out.println("bottom of try");
+
                 } catch (MalformedURLException e) {
+                    System.out.println("eeeeee");
+
                     e.printStackTrace();
                 } catch (ProtocolException e) {
                     e.printStackTrace();
@@ -178,17 +247,23 @@ public class WifiInfoReceiver extends BroadcastReceiver {
         return strBuf.toString();
     }
 
-    public Handler handler = new Handler() {
+    public Handler handlerWifi = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            if (msg.what == MainActivity.LOGIN_SUCCESS) {
+            Button btnLogin = MainActivity.getLoginButton();
+
+            if (msg.what == LOGIN_SUCCESS) {
+                btnLogin.setText("登录成功,再次点击退出");
                 Toast.makeText(context, "登录成功~", Toast.LENGTH_SHORT).show();
-            } else if (msg.what == MainActivity.USERNAME_ERROR) {
+            } else if (msg.what == USERNAME_ERROR) {
+                btnLogin.setText("用户名错误"); //本来想用提示方式更加优美的输入组件的SharedPreferences.Editor，但是传进来后不能用setError（）
                 errorNotification("用户名错误！");
-            } else if (msg.what == MainActivity.PASSWORD_ERROR) {
+            } else if (msg.what == PASSWORD_ERROR) {
+                btnLogin.setText("密码错误");
                 errorNotification("密码错误！");
-            }else if (msg.what == MainActivity.UNSURE){
+            }else if (msg.what == UNSURE){
+                btnLogin.setText("可能登录上了，你试试呗~");
                 errorNotification("可能是其他错误，也可能是正确登陆但是返回的字符串没有上述关键字。");
             }
         }
